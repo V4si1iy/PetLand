c57613da1d8c09062d35718350c427b4a7f39a0f
Vasiliy <pilaroiding@gmail.com>
2023-09-01 21:01:55 +0300

    Initial commit

---
+++ src/main/java/pet/project/PetLand/PetLandApplication.java
@@ -0,0 +1,13 @@
+package pet.project.PetLand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetLandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pet/project/PetLand/PetLandApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.project.PetLand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetLandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
