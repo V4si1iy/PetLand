7163c2422dffcd82d42b812b975762ec8e21d957
mariyatarasenko88 <tmv1231@yandex.ru>
2023-10-08 21:34:20 +0300

    Добавила функцию удаления питомца

--- src/main/java/pet/project/PetLand/service/ShelterService.java
+++ src/main/java/pet/project/PetLand/service/ShelterService.java
@@ -1,5 +1,8 @@
 package pet.project.PetLand.service;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import pet.project.PetLand.repository.ShelterRepository;
 import pet.project.PetLand.model.Shelter;
